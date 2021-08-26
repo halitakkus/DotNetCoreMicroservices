625ad69af3b86829519e37a43878865342397c70
Halit AKKUŞ <halit.akkus-b@tubitak.gov.tr>
2021-08-26 14:07:44 +0300

    Kategori servisi yazıldı. Bazı dto nesneleri değiştirildi.

--- Services/Catalog/FreeCourse.Services.Catalog/Dtos/FeatureDto.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Dtos/FeatureDto.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Catalog.Dtos
 {
-    public class FeatureDto
+    internal class FeatureDto
     {
     }
 }
---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,31 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Models;
+using FreeCourse.Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    interface ICategoryService
+    {
+        /// <summary>
+        /// Lists all categories.
+        /// </summary>
+        /// <returns></returns>
+        Task<Response<List<CategoryDto>>> GetAllAsync();
+
+        /// <summary>
+        /// Adds new category.
+        /// </summary>
+        /// <param name="category"></param>
+        /// <returns></returns>
+        Task<Response<CategoryDto>> CreateAsync(Category category);
+
+        /// <summary>
+        /// Lists categories by id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<Response<CategoryDto>> GetByIdAsync(string id);
+    }
+}
